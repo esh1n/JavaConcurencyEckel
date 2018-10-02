6f6b391fbce62312fa718fe341b99ef01c8d6dd1
sergey_eshin <sergey.eshin@dataart.com>
2018-10-02 11:45:14 +0300

    Initial commit

---
+++ src/com/esh1n/BasicThreads.java
@@ -0,0 +1,10 @@
+package com.esh1n;
+
+public class BasicThreads {
+    public static void main(String[] args){
+        for(int i = 0;i<5;i++){
+            new Thread(new LiftOff(10)).start();
+        }
+        System.out.println("Waiting for LiftOff");
+    }
+}
---
+++ src/com/esh1n/LiftOff.java
@@ -0,0 +1,29 @@
+package com.esh1n;
+
+public class LiftOff implements Runnable{
+
+    protected int countDown = 10;
+    private static int taskCount = 0;
+    private final int id = taskCount++;
+
+    public LiftOff(int countDown){
+        this.countDown = countDown;
+    }
+
+    public String status(){
+        return "#"+ id + "(" + (countDown>0? countDown : "LiftOff") + "), ";
+    }
+
+    @Override
+    public void run() {
+       while (countDown-->0){
+           System.out.println(status());
+           Thread.yield();
+       }
+    }
+
+    public static void main(String[] args){
+        LiftOff launch = new LiftOff(10);
+        launch.run();
+    }
+}
